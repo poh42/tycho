22df8523d74c0150c814b78598476b405336d275
poh42 <45892744+poh42@users.noreply.github.com>
2023-02-08 03:20:44 -0800

    Update urlList.class.js

--- urlList.class.js
+++ urlList.class.js
@@ -1,23 +1,11 @@
-const express = require('express')
-require('dotenv').config();
-const pageListRouter = require('./routers/pageList.router')
+class URL_LIST {
+	constructor(){
+		list: [];
+	}
 
-//2nd attempt create an orbitdb wrapper
+	P_addNewURL(url){
+		this.list.push(url)
+	}
+}
 
-const bodyParser = require('body-parser')
-require('fs-extra');
-
-const app = express()
-
-const PORT = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static('build'))
-
-app.use('/page', pageListRouter);
-
-app.listen(PORT, () => {
-	console.log(`listening on port ${PORT}`)
-})
+module.exports = URL_LIST
